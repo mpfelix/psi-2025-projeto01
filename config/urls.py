9263c4e61b5bd305a8f33306933a29b49df4eb6d
Fernanda84 <fernandaliliam9@gmail.com>
2025-07-15 17:27:20 -0300

    ajustando erros e add texto e imagem

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('website.urls'))
+    path('', include('website.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
